ca2e6d0b6bac2e7eaea4a55f6864f98906dc7446
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-11-05 22:18:57 +0100

    refactor: turn class into a function

--- lab1/src/shingling.py
+++ lab1/src/shingling.py
@@ -1,18 +1,12 @@
 import functools
 
 
-class Shingling:
+@functools.lru_cache()
+def shinglings(document: str, k: int) -> set:
+    result = set()
+    document_length = len(document)
 
-    def __init__(self, k: int, document: str) -> None:
-        self.k = k
-        self.document = document
+    for start in range(document_length - k):
+        result.add(document[start:start + k].__hash__())
 
-    @functools.lru_cache()
-    def shinglings(self) -> set:
-        result = set()
-        document_length = len(self.document)
-
-        for start in range(document_length - self.k):
-            result.add(self.document[start:start + self.k].__hash__())
-
-        return result
+    return result
