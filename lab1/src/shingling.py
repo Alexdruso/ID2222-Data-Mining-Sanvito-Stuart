1303ad3f7033169fe86cb6bcda155228f910b225
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-11-07 13:58:42 +0100

    feat: change hash function to guarantee unique mapping

--- lab1/src/shingling.py
+++ lab1/src/shingling.py
@@ -2,12 +2,27 @@ import functools
 from typing import Set
 
 
-@functools.lru_cache()
-def shingling(document: str, k: int) -> Set[int]:
-    result = set()
-    document_length = len(document)
+class Shingling:
 
-    for start in range(document_length - k):
-        result.add(document[start:start + k].__hash__())
+    def __init__(self) -> None:
+        self._shingling_to_int = dict()
+        self._shingling_number = 0
+        super().__init__()
 
-    return result
+    def hash(self, shingling: str) -> int:
+        if shingling in self._shingling_to_int:
+            return self._shingling_to_int[shingling]
+        else:
+            self._shingling_to_int[shingling] = self._shingling_number
+            self._shingling_number += 1
+            return self._shingling_number - 1
+
+    @functools.lru_cache()
+    def shingling(self, document: str, k: int) -> Set[int]:
+        result = set()
+        document_length = len(document)
+
+        for start in range(document_length - k):
+            result.add(self.hash(document[start:start + k]))
+
+        return result
