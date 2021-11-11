de130f605ddceba68f594d9080a450a61c310964
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-11-11 17:43:10 +0100

    docs: add documentation to compare_signatures

--- lab1/src/compare_signatures.py
+++ lab1/src/compare_signatures.py
@@ -2,4 +2,11 @@ import numpy as np
 
 
 def compare_signatures(A: np.ndarray, B: np.ndarray) -> float:
+    """
+    Estimates the Jaccard similarity between A and B as the probability to have an equal entry in the signatures of A
+    and B.
+    :param A: the minhashed representation of document A
+    :param B: the minhashed representation of document A
+    :return: an estimate of the Jaccard similarity between A and B
+    """
     return sum(A == B) / len(A)
