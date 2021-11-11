e1499b0b9295f374d12cec41ec82346e85642aa0
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-11-11 17:38:30 +0100

    docs: add documentation to compare sets

--- lab1/src/compare_sets.py
+++ lab1/src/compare_sets.py
@@ -2,4 +2,10 @@ from typing import Set
 
 
 def compare_sets(A: Set[int], B: Set[int]) -> float:
+    """
+    This function computes the Jaccard similarity between sets A and B.
+    :param A: set of hashed shingling in document A
+    :param B: set of hashed shingling in document B
+    :return: the Jaccard similarity between A and B
+    """
     return len(A & B) / len(A | B)
