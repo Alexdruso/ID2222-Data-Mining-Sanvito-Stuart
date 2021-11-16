c8f698737a08f74512b33d6210f99a82417374f2
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-11-16 13:47:42 +0100

    refactor: extract function to read dataset

--- lab2/src/a_priori.py
+++ lab2/src/a_priori.py
@@ -1,4 +1,33 @@
-from typing import Tuple, Dict
+from typing import Tuple, Dict, List, Set
+from collections import defaultdict
+
+
+def read_dataset(file: str) -> Tuple[List[Set[int]], int]:
+    """
+    This function reads from a file .dat assuming that on every row of the file there is a basket of items and returns
+    the list of the baskets and the maximum basket size.
+
+    :param file: the path to the input file
+    :return: a tuple containing the list of baskets and the maximum basket size
+    """
+
+    with open(file, "r") as f:
+        baskets : List[Set[int]] = list(
+            map(
+                lambda basket: {int(item_id) for item_id in basket.split()},
+                f.read().splitlines()
+            )
+        )
+    f.close()
+
+    largest_item_set_size : int = max(
+        map(
+            len,
+            baskets
+        )
+    )
+
+    return baskets, largest_item_set_size
 
 
 def find_frequent_item_sets(file: str, s: int = 1) -> Dict[Tuple[int, ...], int]:
@@ -14,23 +43,10 @@ def find_frequent_item_sets(file: str, s: int = 1) -> Dict[Tuple[int, ...], int]
 
     frequent_item_sets: Dict[Tuple[int, ...], int] = {}
 
-    with open(file, "r") as f:
-        baskets = list(
-            map(
-                lambda basket: {int(item_id) for item_id in basket.split()},
-                f.read().splitlines()
-            )
-        )
-    f.close()
-
-    largest_item_set_size = max(
-        map(
-            len,
-            baskets
-        )
-    )
+    baskets, largest_item_set_size = read_dataset(file=file)
 
     return frequent_item_sets
+
 
 if __name__ == "__main__":
     print(
