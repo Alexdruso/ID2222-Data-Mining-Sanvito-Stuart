c7f9fb09f4af041fd9cb9a8c8dfb12e11b3b8703
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-11-16 17:52:08 +0100

    feat: implement generation frequent singletons

--- lab2/src/a_priori.py
+++ lab2/src/a_priori.py
@@ -12,7 +12,7 @@ def read_dataset(file: str) -> Tuple[List[Set[int]], int]:
     """
 
     with open(file, "r") as f:
-        baskets : List[Set[int]] = list(
+        baskets: List[Set[int]] = list(
             map(
                 lambda basket: {int(item_id) for item_id in basket.split()},
                 f.read().splitlines()
@@ -20,7 +20,7 @@ def read_dataset(file: str) -> Tuple[List[Set[int]], int]:
         )
     f.close()
 
-    largest_item_set_size : int = max(
+    largest_item_set_size: int = max(
         map(
             len,
             baskets
@@ -28,6 +28,29 @@ def read_dataset(file: str) -> Tuple[List[Set[int]], int]:
     )
 
     return baskets, largest_item_set_size
+
+
+def find_frequent_singletons(baskets: List[Set[int]], s: int = 1) -> Dict[Tuple[int], int]:
+    """
+    This function finds all the items having a support greater than s across all the baskets.
+
+    :param baskets: the list of all baskets represented as sets
+    :param s: the threshold support to consider an item as frequent
+    :return: the set of all frequent singletons
+    """
+
+    item_to_support = defaultdict(int)
+
+    for basket in baskets:
+        for item in basket:
+            item_to_support[(item,)] += 1
+
+    return dict(
+        filter(
+            lambda element: element[1] > s,
+            item_to_support.items()
+        )
+    )
 
 
 def find_frequent_item_sets(file: str, s: int = 1) -> Dict[Tuple[int, ...], int]:
@@ -44,6 +67,13 @@ def find_frequent_item_sets(file: str, s: int = 1) -> Dict[Tuple[int, ...], int]
     frequent_item_sets: Dict[Tuple[int, ...], int] = {}
 
     baskets, largest_item_set_size = read_dataset(file=file)
+
+    frequent_singletons = find_frequent_singletons(baskets=baskets, s=s)
+    frequent_item_sets.update(frequent_singletons)
+
+    precedent_frequent_item_sets = frequent_singletons.keys()
+    for _ in range(1, largest_item_set_size + 1):
+        print('kek')
 
     return frequent_item_sets
 
