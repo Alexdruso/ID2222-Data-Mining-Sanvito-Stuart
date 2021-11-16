bbc212485a98ad5d2c68fc3078eec21a6a4f0ca7
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-11-16 18:39:36 +0100

    feat: implement a priori algorithm

--- lab2/src/a_priori.py
+++ lab2/src/a_priori.py
@@ -64,7 +64,7 @@ def generate_candidate_item_sets(
 ) -> Set[Tuple[int, ...]]:
     """
     This function returns the set of candidate new frequent itemsets for step k+1 of the a priori algorithm
-     by combining the itemsets found at step k of the algorithm with frequent singletons.
+    by combining the itemsets found at step k of the algorithm with frequent singletons.
 
     :param precedent_item_sets: the frequent itemsets found at time k of the algorithm
     :param frequent_singletons: the frequent singletons in the dataset
@@ -79,15 +79,16 @@ def generate_candidate_item_sets(
 
 
 def filter_frequent_item_sets(
-        baskets,
-        candidate_item_sets,
-        s : int = 1
+        baskets: List[Set[int]],
+        candidate_item_sets: Set[Tuple[int, ...]],
+        s: int = 1
 ) -> Dict[Tuple[int, ...], int]:
     item_set_to_support = defaultdict(int)
 
     for basket in baskets:
         for candidate_item_set in candidate_item_sets:
-
+            if set(candidate_item_set).issubset(basket):
+                item_set_to_support[candidate_item_set] += 1
 
     return dict(
         filter(
@@ -99,15 +100,17 @@ def filter_frequent_item_sets(
 
 def find_frequent_item_sets(
         file: str,
-        s: int = 1
+        s: int = 1,
+        maximum_item_set_size: int = None
 ) -> Dict[Tuple[int, ...], int]:
     """
     This function reads from a file .dat assuming that on every row of the file there is a basket of items.
-    The function then generates the set of frequent itemsets having support greater or equal than s with the apriori
-    algorithm.
+    The function then generates the set of frequent itemsets having support greater or equal than s and maximum size
+    equal to maximum_item_set_size with the apriori algorithm.
 
     :param file: the path to the input file
     :param s: the minimum support required to consider an itemset frequent
+    :param maximum_item_set_size: the maximum size of the frequent itemsets, if None it is the maximum size of a basket
     :return: the set of all frequent itemsets, represented as tuples, mapped to their support
     """
 
@@ -115,12 +118,15 @@ def find_frequent_item_sets(
 
     baskets, largest_item_set_size = read_dataset(file=file)
 
+    maximum_item_set_size = min(maximum_item_set_size, largest_item_set_size) if maximum_item_set_size is not None \
+        else largest_item_set_size
+
     frequent_singletons = find_frequent_singletons(baskets=baskets, s=s)
     frequent_item_sets.update(frequent_singletons)
     frequent_singletons = set(frequent_singletons.keys())
 
     precedent_frequent_item_sets = frequent_singletons
-    for _ in range(1, largest_item_set_size + 1):
+    for _ in range(2, maximum_item_set_size + 1):
         candidate_item_sets = generate_candidate_item_sets(
             precedent_item_sets=precedent_frequent_item_sets,
             frequent_singletons=frequent_singletons
@@ -128,7 +134,8 @@ def find_frequent_item_sets(
 
         new_frequent_item_sets = filter_frequent_item_sets(
             baskets=baskets,
-            candidate_item_sets=candidate_item_sets
+            candidate_item_sets=candidate_item_sets,
+            s=s
         )
 
         frequent_item_sets.update(new_frequent_item_sets)
@@ -139,5 +146,9 @@ def find_frequent_item_sets(
 
 if __name__ == "__main__":
     print(
-        find_frequent_item_sets(file='../data/T10I4D100K.dat', s=10)
+        find_frequent_item_sets(
+            file='../data/T10I4D100K.dat',
+            s=1,
+            maximum_item_set_size=2
+        )
     )
