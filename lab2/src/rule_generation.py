17ff4d2d1dd9757b1737fc2b231edb592f9a5756
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-11-19 17:22:19 +0100

    feat: implement rule generation

--- lab2/src/rule_generation.py
+++ lab2/src/rule_generation.py
@@ -1,16 +1,42 @@
-from typing import Tuple, Dict, List, Set
+from collections import defaultdict
+from itertools import combinations
+from typing import Dict, Set, FrozenSet
 
 
 def generate_rules(
-        frequent_item_sets: Dict[Set[int], int],
-        s: int = 1,
+        frequent_item_sets: Dict[FrozenSet[int], int],
         c: float = 0.1
-) -> Dict[Set[int], Set[Set[int]]]:
+) -> Dict[FrozenSet[int], Set[FrozenSet[int]]]:
     """
     This function returns all the association rules having support greater than s and confidence greater than c.
 
     :param frequent_item_sets: the dictionary of all frequent itemsets with their respective support
-    :param s: the threshold support
     :param c: the threshold confidence
     :return: a dictionary linking each left side itemset of the association rule with all the right side itemsets
     """
+
+    association_rules: Dict[FrozenSet[int], Set[FrozenSet[int]]] = defaultdict(set)
+
+    for item_set in filter(lambda x: len(x) > 1, frequent_item_sets.keys()):
+        for antecedent_length in range(1, len(item_set)):
+            for antecedent in [frozenset(combination) for combination in combinations(item_set, antecedent_length)]:
+                confidence = frequent_item_sets[item_set] / frequent_item_sets[antecedent]
+
+                if confidence >= c:
+                    consequent = item_set - antecedent
+                    association_rules[antecedent].add(consequent)
+
+    return association_rules
+
+
+if __name__ == "__main__":
+    print(
+        generate_rules(
+            {
+                frozenset((0,)): 10,
+                frozenset((1,)): 10,
+                frozenset((0, 1)): 10
+            },
+            0.1
+        )
+    )
