298156b4b451765796d38c07c3be0cb6fe4247af
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-12-08 13:05:04 +0100

    feat: factory enum

--- lab5/src/main/java/se/kth/jabeja/annealing/AnnealingType.java
+++ lab5/src/main/java/se/kth/jabeja/annealing/AnnealingType.java
@@ -1,4 +1,18 @@
 package se.kth.jabeja.annealing;
 
 public enum AnnealingType {
+    LINEAR {
+        @Override
+        public Annealer getAnnealer(float temperature, float delta, float alpha) {
+            return new LinearAnnealer(temperature, delta, alpha);
+        }
+    },
+    EXPONENTIAL {
+        @Override
+        public Annealer getAnnealer(float temperature, float delta, float alpha) {
+            return new ExponentialAnnealer(temperature, delta, alpha);
+        }
+    };
+
+    public abstract Annealer getAnnealer(float temperature, float delta, float alpha);
 }
