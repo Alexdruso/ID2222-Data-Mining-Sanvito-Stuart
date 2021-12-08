e4d4bf3a88cacd6cdde00bd66ddceb0fedc62aa1
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-12-08 09:43:59 +0100

    feat: implement linear annealer

--- lab5/src/main/java/se/kth/jabeja/annealing/LinearAnnealer.java
+++ lab5/src/main/java/se/kth/jabeja/annealing/LinearAnnealer.java
@@ -2,6 +2,8 @@ package se.kth.jabeja.annealing;
 
 import se.kth.jabeja.Node;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Optional;
 
@@ -12,6 +14,32 @@ public class LinearAnnealer extends Annealer {
 
     @Override
     public Optional<Node> findPartner(Node node, Node[] candidates, HashMap<Integer, Node> entireGraph) {
-        return Optional.empty();
+        return Arrays.stream(candidates)
+                .filter(
+                        candidate -> temperature * getCost(
+                                node,
+                                candidate.getColor(),
+                                candidate,
+                                node.getColor(),
+                                entireGraph
+                        ) > getCost(
+                                node,
+                                node.getColor(),
+                                candidate,
+                                candidate.getColor(),
+                                entireGraph
+                        )
+                )
+                .max(
+                        Comparator.comparingDouble(
+                                candidate -> getCost(
+                                        node,
+                                        candidate.getColor(),
+                                        candidate,
+                                        node.getColor(),
+                                        entireGraph
+                                )
+                        )
+                );
     }
 }
