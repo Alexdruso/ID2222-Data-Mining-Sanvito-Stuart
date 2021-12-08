5044b15e03f79e49c08868c776511d8b4db55974
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-12-08 17:58:27 +0100

    refactor: optimize imports

--- lab5/src/main/java/se/kth/jabeja/annealing/CustomAnnealer.java
+++ lab5/src/main/java/se/kth/jabeja/annealing/CustomAnnealer.java
@@ -1,12 +1,8 @@
 package se.kth.jabeja.annealing;
 
-import java.util.Random;
-
 import static java.lang.Math.exp;
-import static java.lang.Math.log;
 
 public class CustomAnnealer extends NonLinearAnnealer{
-    private final Random randomGenerator = new Random();
 
     CustomAnnealer(float temperature, float delta, float alpha) {
         super(temperature, delta, alpha);
